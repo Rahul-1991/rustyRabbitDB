bb61a52e6e5cf66583eaf32a381710c013b68d44
Rahul <rahul1991.c@gmail.com>
2023-11-13 10:17:58 +0530

    added different SET options for TTL

--- commands/set.go
+++ commands/set.go
@@ -3,14 +3,25 @@ package commands
 import (
 	"goredis/store"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func Set(commandArgList []string) string {
 	var ttl int64 = -1
-	if len(commandArgList) > 3 && (commandArgList[3] == "EX" || commandArgList[3] == "ex") {
-		ttl, _ = strconv.ParseInt(commandArgList[4], 10, 64)
-		ttl += time.Now().Unix()
+	if len(commandArgList) > 3 {
+		if strings.ToLower(commandArgList[3]) == "ex" {
+			ttl, _ = strconv.ParseInt(commandArgList[4], 10, 64)
+			ttl += time.Now().Unix()
+		} else if strings.ToLower(commandArgList[3]) == "px" {
+			ttl, _ = strconv.ParseInt(commandArgList[4], 10, 64)
+			ttl = (ttl / 1000) + time.Now().Unix()
+		} else if strings.ToLower(commandArgList[3]) == "exat" {
+			ttl, _ = strconv.ParseInt(commandArgList[4], 10, 64)
+		} else if strings.ToLower(commandArgList[3]) == "pxat" {
+			ttl, _ = strconv.ParseInt(commandArgList[4], 10, 64)
+			ttl /= 1000
+		}
 	}
 	valMap := store.CacheItem{
 		Value: commandArgList[2],
